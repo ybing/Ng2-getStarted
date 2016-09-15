4ebf8994f34cfecc0ece3bf664ecc354dcc7123b
张亚兵 <zyb9193@sina.com>
2016-09-15 14:08:15 +0800

    delete file

--- app/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/**
- * Created by yabing on 16/9/13.
- */
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_module_1 = require('./ts/app.module');
-var platform = platform_browser_dynamic_1.platformBrowserDynamic();
-platform.bootstrapModule(app_module_1.AppModule);
-//# sourceMappingURL=main.js.map
